6b0c96fd504c69c259de75fb3581298b79427f0d
Avand Amiri <avand@avandamiri.com>
2011-12-27 22:30:39 -0500

    Customizing 960gridder

---
+++ source/js/960gridder.js
@@ -0,0 +1,16 @@
+var gOverride = {
+  gColor: '#EEEEEE',
+  gColumns: 12,
+  gOpacity: 0.35,
+  gWidth: 10,
+  pColor: '#C0C0C0',
+  pHeight: 20,
+  pOffset: 0,
+  pOpacity: 0.55,
+  center: true,
+  gEnabled: true,
+  pEnabled: true,
+  setupEnabled: true,
+  fixFlash: true,
+  size: 960
+};
