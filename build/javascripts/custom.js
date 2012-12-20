f581661a3780f6bd8f933e2e607bf3814a96de2a
Avand Amiri <avand@avandamiri.com>
2012-12-19 20:16:33 -0500

    Rebuilding

--- build/javascripts/custom.js
+++ build/javascripts/custom.js
@@ -2,34 +2,14 @@ $.backstretch($('meta[name=background]').attr('content'), {
   speed: 600, centeredX: false, centeredY: false
 });
 
-function slideDownSubnavigation(element) {
-  $("#centennial").stop().animate({ opacity: 0.0 }, { queue: false });
-
-  element.stop().css('z-index', '1000').show().animate({
-    height: element.data('height')
-  }, {
-    queue: false,
-    duration: 1000,
-    easing: 'easeOutBounce',
-    complete: function() {
-      $("#centennial").stop().animate({ opacity: 0.0 }, { queue: false });
-    }
-  })
+function showSubnavigation(element) {
+  element.css('height', element.data('height')).show();
+  centennial.hide();
 };
 
-function slideUpSubnavigation(element) {
-  element.stop().css('z-index', '999').animate({
-    height: 0
-  }, {
-    queue: false,
-    duration: 1000,
-    easing: 'easeOutSine',
-    complete: function() {
-      $(this).hide();
-      $("#centennial").animate({ opacity: 1.0 }, { queue: false });
-    }
-  })
-
+function hideSubnavigation(element) {
+  element.hide();
+  centennial.show();
 }
 
 function adjustMenuPadding(current, currentPadding, prevPaddingRight, nextPaddingLeft) {
@@ -55,7 +35,9 @@ function decreaseMenuItemPadding(element) {
   adjustMenuPadding(element, '25px', '25px', '25px')
 };
 
-$(document).ready(function(){
+$(document).ready(function() {
+  centennial = $("#centennial");
+
   $('.subnavigation').each(function() {
     $(this).data('height', $(this).height());
     $(this).css('height', 0);
@@ -63,17 +45,17 @@ $(document).ready(function(){
 
   $('.menu-item').mouseover(function() {
     increaseMenuItemPadding($(this));
-    slideDownSubnavigation($('#' + $(this).data('name')));
+    showSubnavigation($('#' + $(this).data('name')));
   }).mouseout(function() {
     decreaseMenuItemPadding($(this));
-    slideUpSubnavigation($('#' + $(this).data('name')));
+    hideSubnavigation($('#' + $(this).data('name')));
   })
 
   $('.subnavigation').mouseover(function() {
     increaseMenuItemPadding($('.menu-item[data-name="' + $(this).attr('id') + '"]'));
-    slideDownSubnavigation($(this));
+    showSubnavigation($(this));
   }).mouseout(function() {
     decreaseMenuItemPadding($('.menu-item[data-name="' + $(this).attr('id') + '"]'));
-    slideUpSubnavigation($(this));
+    hideSubnavigation($(this));
   })
 });
