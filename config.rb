69a74875839a1c5d3deaa17f3b4f6e162df85141
Avand Amiri <avand@avandamiri.com>
2017-02-28 00:22:03 -0800

    Adding a helpful carousel method

--- config.rb
+++ config.rb
@@ -115,4 +115,46 @@ helpers do
       </div>
     HTML
   end
+
+  def carousel(name, *images)
+    id = "carousel-#{name.gsub(' ', '-')}"
+    slides = ""
+    indicators = ""
+
+    images.each_with_index do |image, i|
+      slides += <<-HTML
+        <div class="item #{'active' if i.zero?}">
+          <img src="#{image}" alt="#{name} picture #{i + 1}">
+        </div>
+      HTML
+
+      indicators += <<-HTML
+        <li data-target="##{id}" data-slide-to="#{i}" #{'class="active"' if i.zero?}></li>
+      HTML
+    end
+
+    <<-HTML
+      <div id="#{id}" class="carousel slide" data-ride="carousel" data-interval="false">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          #{indicators}
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          #{slides}
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="##{id}" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="##{id}" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    HTML
+  end
 end
