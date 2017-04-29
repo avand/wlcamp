5b0b6e772d65f8b7937b3e8214a20c99c7ff857b
Avand Amiri <avand@avandamiri.com>
2017-04-29 14:58:50 -0700

    Removing Backstretch lib and some JS cleanup

--- source/javascripts/custom.js
+++
@@ -1,9 +0,0 @@
-var centennialBanner = null;
-
-var background = $('meta[name=background]').attr('content');
-
-if (background) {
-  $.backstretch(background, {
-    speed: 600, centeredX: false, centeredY: false
-  });
-}
